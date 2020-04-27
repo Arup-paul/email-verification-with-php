6be597b6e2a05ce0b1252ad3ee60befb26dd62c6
Arup <arupctg@gmail.com>
2020-04-27 01:32:26 -0800

    succesfully done login page

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
 require_once "controllers/authController.php";
 
+if(!isset($_SESSION['id'])){
+    header('location:login.php');
+    exit();
+}
 ?>
 
 
@@ -13,7 +17,7 @@ require_once "controllers/authController.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -24,27 +28,44 @@ require_once "controllers/authController.php";
       <div class="container">
           <div class="row">
               <div class="col-4 offset-4 form-div login">
-                <div class="alert alert-success">
-                    You are now logged In
+              <?php
+                if ( isset( $_SESSION['message'] ) ): ?>
+                <div class="alert  <?php echo $_SESSION['alert-class']; ?>">
+                   <?php
+                    echo $_SESSION['message'];
+                    unset( $_SESSION['message'] );
+                    unset( $_SESSION['alert-class'] );
+                    ?>
                 </div>
-                 <h3>Welcome,Arup  paul</h3>
-                 <a href="#" class="logout">logout</a>
+                  <?php endif;?>
+                 <h3>Welcome, <?php echo $_SESSION['username']; ?></h3>
+                 <a href="index.php?logout=1" class="logout">logout</a>
+
+
+                <?php
+                     if ( !$_SESSION['verified'] ): ?>
+
 
                  <div class="alert alert-warning">
                      You Need to verify your account <div class="">Sign in to your email account and click on the verification link we just emailed you at
-                         <strong>aruppaulctg@gmail.com</strong>
+                         <strong> <?php echo $_SESSION['email']; ?></strong>
                      </div>
                  </div>
+                  <?php endif;?>
+
+                  <?php if ( $_SESSION['verified'] ): ?>
 
                      <button class="btn btn-block btn-lg btn-primary">
                          Verified
                      </button>
-               
+                     <?php endif;?>
+
+
               </div>
           </div>
       </div>
 
-   
-    
+
+
 </body>
-</html> 
+</html>
--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+
+<?php
+
+require_once "controllers/authController.php";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -17,23 +25,27 @@
           <div class="row">
               <div class="col-4 offset-4 form-div login">
                   <form action="login.php" method="post">
-                      <h3 class="text-center">Login</h3> 
+                      <h3 class="text-center">Login</h3>
 
-                       <!-- <div class="alert alert-danger">
-                           <li>Username required</li>
-                       </div> -->
+                      <?php if ( count( $errors ) > 0 ) {?>
+                       <div class="alert alert-danger">
+                            <?php foreach ( $errors as $error ) {?>
+                           <li><?php echo $error; ?></li>
+                            <?php }?>
+                       </div>
+                            <?php }?>
 
-             
 
-                      
+
+
                       <div class="form-group">
                         <label for="email">Username or Email</label>
-                        <input type="email" name="email" class="form-control form-control-lg">  
+                        <input type="text" name="username" class="form-control form-control-lg">
                       </div>
 
                       <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control form-control-lg">  
+                        <input type="password" name="password" class="form-control form-control-lg">
                       </div>
 
 
@@ -48,7 +60,7 @@
           </div>
       </div>
 
-   
-    
+
+
 </body>
 </html>
