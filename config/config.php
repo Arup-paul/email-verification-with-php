4a3fda7951bc8d726fbcb80158d2f4409ad3f013
Arup <arupctg@gmail.com>
2020-04-29 00:47:02 -0800

    change

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php 
 
 
-define('EMAIL','aruppaulctg@gmail.com');
-define('PASSWORD','@Aruppaul192001222');
+define('EMAIL','');
+define('PASSWORD','');
 
 define('DB_HOST','localhost');
 define('DB_USER','root');
